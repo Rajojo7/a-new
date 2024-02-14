6e3c9fe98cca0d5126137979a7ccdf33ca8b27a7
Rajojo7 <rajosangno5@gmail.com>
2024-02-14 16:54:50 +0530

    addes hello.cpp file

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=20;i++){
+        for(int j=1;j<=20;j++){
+            if(i==1 || i==20 || j==1 || j==20){
+                cout << "#";
+            }else{
+                cout << " ";
+            }
+        }
+    }
+    return 0;
+}
